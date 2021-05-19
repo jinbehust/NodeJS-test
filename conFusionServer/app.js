627f7fd185c4d5076bdce54a2f464456d47bb241
Giang Nguyen Duc <ducgiangbkhn@gmail.com>
2021-05-19 11:36:25 +0700

    Express Session Part 1
    Express Session Part 2 temp
    
    Express Session Part 2
    
    Fix 16:24
    
    16:50
    
    17:05

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -1,8 +1,10 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 const mongoose = require('mongoose');
 const indexRouter = require('./routes/index');
@@ -35,49 +37,40 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345 - 67890-09876-54321'));
+// app.use(cookieParser('12345 - 67890-09876-54321'));
 
-function authFunction(req, res, next) {
-  if (!req.signedCookies.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-    }
-
-    // eslint-disable-next-line new-cap
-    const auth = new Buffer.from(authHeader.split(' ')[1], 'base64')
-      .toString()
-      .split(':');
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === 'admin' && pass === 'password') {
-      res.cookie('user', 'admin', { signed: true });
-      next(); // authorized
-    } else {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-    }
-  } else if (req.signedCookies.user === 'admin') {
-    next();
-  } else {
-    const err = new Error('You are not authenticated!');
-    err.status = 401;
-    next(err);
-  }
-}
-
-app.use(authFunction);
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    name: 'session-id',
+    secret: '12345 - 67890-09876-54321',
+    saveUninitialized: false,
+    resave: false,
+    store: new FileStore(),
+  }),
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+const auth = (req, res, next) => {
+  if (!req.session.user) {
+    const err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+  }
+  if (req.session.user === 'authenticated') {
+    next();
+  } else {
+    const err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+  }
+};
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
