c06a45cc3a55fb8e69c22410c205a6b8ddd97f90
Giang Nguyen Duc <ducgiangbkhn@gmail.com>
2021-05-21 15:55:31 +0700

    Mongoose

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -6,28 +6,31 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
+router.get('/', (_req, res, next) => {
   res.send('respond with a resource');
 });
 
-router.post('/signup', (req, res, next) => {
-  User.register(
-    new User({ username: req.body.username }),
-    req.body.password,
-    (err) => {
-      if (err) {
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({ err });
-      } else {
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({ success: true, status: 'Registration Successfull!' });
-        });
-      }
-    },
-  );
+router.post('/signup', async (req, res, next) => {
+  try {
+    const user = await User.register(
+      new User({ username: req.body.username }),
+      req.body.password,
+    );
+    if (req.body.firstname && req.body.lastname) {
+      user.firstname = req.body.firstname;
+      user.lastname = req.body.lastname;
+    }
+    await user.save();
+    passport.authenticate('local')(req, res, () => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({ success: true, status: 'Registration Successful!' });
+    });
+  } catch (err) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({ err });
+  }
 });
 
 router.post('/login', passport.authenticate('local'), (req, res, next) => {
